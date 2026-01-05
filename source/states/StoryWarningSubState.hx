3ea43401c658d40169aeda5e016175db98f53721
itsoutchy-projects <itsoutchy55@yahoo.com>
2026-01-05 18:52:19 +0000

    Add warning for suicide and other sensitive topics

--- source/states/StoryWarningSubState.hx
+++ source/states/StoryWarningSubState.hx
@@ -17,6 +17,8 @@ class StoryWarningSubstate extends MusicBeatSubstate
 
 	var vignette:FlxSprite;
 
+	var warningidx = 0;
+
 	override function create()
 	{
 		super.create();
@@ -29,8 +31,8 @@ class StoryWarningSubstate extends MusicBeatSubstate
 		warnText = new FlxText(0, 0, FlxG.width,
 			'Sup! This mod might not show the whole story, so:\n
 			-----------------------------------------------\n
-			Press ENTER to view the webcomic\n
-			Press SPACE to watch the short film\n
+			Press ENTER to view the webcomic.\n
+			Press SPACE to watch the short film.\n
 			Press ESCAPE to proceed anyway.\n
 			-----------------------------------------------\n
 			Thank you for playing this mod!',
@@ -42,6 +44,7 @@ class StoryWarningSubstate extends MusicBeatSubstate
 		add(warnText);
 		
 		vignette = new FlxSprite(0, 0, Paths.image("vignette"));
+		vignette.alpha = 0.5;
 		add(vignette);
 
 		FlxTween.tween(bg, { alpha: 0.8 }, 0.6, { ease: FlxEase.sineIn });
@@ -60,7 +63,22 @@ class StoryWarningSubstate extends MusicBeatSubstate
 				CoolUtil.browserLoad("https://www.youtube.com/watch?v=mSapwq1sV9k");
 			}
 			else if(controls.BACK) {
-				leftState = true;
+				if (warningidx != 0) {
+					leftState = true;
+				} else {
+					warnText.text =
+					'This mod may also feature sensitive topics such as:\n
+					Suicide\n
+					Loneliness\n
+					Isolation\n
+					Self-harm\n
+					Breakdowns\n
+					If you or anyone you know are affected by these issues, please talk to a trusted family member.\n
+					Remember: you are not alone.\n
+					Press ESCAPE to continue anyway. You have been warned.';
+					//warnText.draw();
+					warningidx = 1;
+				}
 			}
 			if(leftState)
 			{
