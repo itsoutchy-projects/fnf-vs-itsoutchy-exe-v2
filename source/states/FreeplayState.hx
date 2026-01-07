1001519463afb6fbea394a38f035f234d5bba2fd
itsoutchy-projects <itsoutchy55@yahoo.com>
2026-01-07 21:06:48 +0000

    change freeplay bg

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -116,7 +116,7 @@ class FreeplayState extends MusicBeatState
 		}
 		Mods.loadTopMod();
 
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg = new FlxSprite().loadGraphic(Paths.image('titleBG'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;
 		add(bg);
 		bg.screenCenter();
