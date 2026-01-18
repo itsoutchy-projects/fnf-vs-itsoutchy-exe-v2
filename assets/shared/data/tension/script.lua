76e54335cb5614c30b3b9adc125165413d797411
itsoutchy-projects <itsoutchy55@yahoo.com>
2026-01-18 13:10:16 +0000

    show growing tension

--- assets/shared/data/tension/script.lua
+++ assets/shared/data/tension/script.lua
@@ -1,3 +1,17 @@
 function onCreate()
     triggerEvent("HUD", "1");
 end
+
+function onStepHit()
+    if curStep == 1088 then
+        makeLuaSprite("redVignette", "red vignette");
+        setObjectCamera("redVignette", "hud");
+        addLuaSprite("redVignette", true);
+    end
+    if curStep == 1408 then
+        doTweenAlpha("redVigAlphLower", "redVignette", 0.3, 0.1);
+    end
+    if curStep == 1920 then
+        doTweenAlpha("redVigAlphHightened", "redVignette", 1, 0.1);
+    end
+end
