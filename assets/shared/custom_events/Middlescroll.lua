aa6a7d300a1618e1c1afe0c4be58b3da7376cc1a
itsoutchy-projects <itsoutchy55@yahoo.com>
2026-05-29 13:46:48 +0100

    dont want middlescroll to just end up being a cheat. make it so if middlescroll is already on, this turns it off, and vice versa

--- assets/shared/custom_events/Middlescroll.lua
+++ assets/shared/custom_events/Middlescroll.lua
@@ -50,15 +50,17 @@ function onCreatePost()
     }
 end
 
-function onEvent(event, value1, value2, strumTime)
-    if event == "Middlescroll" then
-        if value2 == "" then
-            durationTween = middlescrollTweenDuration;
-        else
-            durationTween = value2;
+function changeMiddlescroll(onoff)
+    local final = onoff;
+    if (onoff == 0 or onoff == "0") and not middlescroll then
+        final = 1;
+    else
+        if (onoff == 1 or onoff == "1") and middlescroll then
+            final = 0;
         end
-        debugPrint(value2 == "");
-        if value1 == "0" then
+    end
+    debugPrint(final);
+    if final == 0 or final == "0" then
             -- off
             middlescrollchanged = false;
             -- Credit to Draconic-Hybrid on Github
@@ -77,27 +79,32 @@ function onEvent(event, value1, value2, strumTime)
             for i = 0, getProperty('opponentStrums.length') do
                 noteTweenAlpha("opponentStrumsFadeIn"..i, i, 1, durationTween)
             end
+    end
+    if final == 1 or final == "1" then
+        for i = 0, getProperty('opponentStrums.length') do
+            --setPropertyFromGroup('opponentStrums', i, 'x', getStrumDefaultX(getProperty('opponentStrums.length') + i) - 320);
+            noteTweenX("opponentStrumsMiddleScrollOn"..i, i, getStrumDefaultX(getProperty('opponentStrums.length') + i) - 320, durationTween);
+            noteTweenX("playerStrumsMiddleScrollOn"..i, getProperty('opponentStrums.length') + i, getStrumDefaultX(getProperty('playerStrums.length') + i) - 320, durationTween);
+            --setPropertyFromGroup('playerStrums', i, 'x', getStrumDefaultX(getProperty('playerStrums.length') + i) - 320);
+            noteTweenAlpha("opponentStrumsFadeOut"..i, i, 0.1, durationTween)
+            --doTweenAlpha("opponentStrumsFadeOut"..i, "opponentStrums", 0.3, middlescrollTweenDuration);
+            --setPropertyFromGroup('opponentStrums', i, 'alpha', 0.3);
         end
-        if value1 == "1" then
-            -- on
+        -- for i = 0, getProperty('playerStrums.length') do
+        --     setPropertyFromGroup('playerStrums', i, 'x', getPropertyFromGroup("opponentStrums", i, "x"));
+        -- end
+        middlescrollchanged = true;
+    end
+end
 
-            -- if you plan on sharing this script/putting it in your mod, make sure to check if the player
-            -- has middlescroll enabled.
-            if not middlescroll then
-                for i = 0, getProperty('opponentStrums.length') do
-                    --setPropertyFromGroup('opponentStrums', i, 'x', getStrumDefaultX(getProperty('opponentStrums.length') + i) - 320);
-                    noteTweenX("opponentStrumsMiddleScrollOn"..i, i, getStrumDefaultX(getProperty('opponentStrums.length') + i) - 320, durationTween);
-                    noteTweenX("playerStrumsMiddleScrollOn"..i, getProperty('opponentStrums.length') + i, getStrumDefaultX(getProperty('playerStrums.length') + i) - 320, durationTween);
-                    --setPropertyFromGroup('playerStrums', i, 'x', getStrumDefaultX(getProperty('playerStrums.length') + i) - 320);
-                    noteTweenAlpha("opponentStrumsFadeOut"..i, i, 0.1, durationTween)
-                    --doTweenAlpha("opponentStrumsFadeOut"..i, "opponentStrums", 0.3, middlescrollTweenDuration);
-                    --setPropertyFromGroup('opponentStrums', i, 'alpha', 0.3);
-                end
-                -- for i = 0, getProperty('playerStrums.length') do
-                --     setPropertyFromGroup('playerStrums', i, 'x', getPropertyFromGroup("opponentStrums", i, "x"));
-                -- end
-                middlescrollchanged = true;
-            end
+function onEvent(event, value1, value2, strumTime)
+    if event == "Middlescroll" then
+        if value2 == "" then
+            durationTween = middlescrollTweenDuration;
+        else
+            durationTween = value2;
         end
+        changeMiddlescroll(value1);
+        debugPrint(value2 == "");
     end
 end
